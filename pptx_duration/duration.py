ebca11f0480963633189849e0318d2d8913db4dd
danlindleybaker <danlindleybaker@gmail.com>
2023-01-19 12:56:16 +0000

    initial commit

---
+++ pptx_duration/duration.py
@@ -0,0 +1,31 @@
+from tinytag import TinyTag
+import glob
+import datetime
+from zipfile import ZipFile
+import shutil
+import os
+import sys
+
+def main():
+    path = sys.argv[1]
+    folder, _ = os.path.split(path)
+    temp_folder = folder + "\\temp"
+
+    with ZipFile(path, 'r') as zObject:
+        zObject.extractall(temp_folder)
+
+    file_list = glob.glob(temp_folder + "\\ppt\\media\\" + "*.m4a")
+
+    total_time = 0
+    for file in file_list:
+        tag = TinyTag.get(file)
+        total_time += tag.duration
+
+    print(f"Total Time = {datetime.timedelta(seconds=total_time)}")
+
+    shutil.rmtree(temp_folder)
+
+
+
+if __name__ == "__main__":
+    main()

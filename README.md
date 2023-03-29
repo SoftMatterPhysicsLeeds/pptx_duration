8253a6d3c672c8b267533c7693ca38d6731bb59c
danlindleybaker <danlindleybaker@gmail.com>
2023-03-29 10:01:52 +0100

    pathlib instead of os.path etc

---
+++ README.md
@@ -0,0 +1,22 @@
+## What is this? 
+
+Have you ever sat through a pre-recorded Powerpoint presentation (3P) and wondered
+when it would end? Would you like to put a 3P in the bin immediately if it is
+longer that the 'maximum time' limit set in the candidate brief? Have you ever
+made a 3P and wondered whether it would annoy the interviewers because of its
+length? 
+
+## pptx_duration
+
+Pre-recorded Powerpoint presentations do not automatically come with a 'timer'
+and so it isn't immediately obvious how long a recorded presentation is. This
+simple Python script unzips the .pptx and determines the runtime for you. 
+
+## Usage
+```
+pip install pptx_duration
+python pptx_duration PATH_TO_PPTX
+
+Total Time = 0:10:00
+
+```
--- pptx_duration/duration.py
+++ pptx_duration/duration.py
@@ -1,20 +1,24 @@
 from tinytag import TinyTag
-import glob
 import datetime
+from pathlib import Path
 from zipfile import ZipFile
 import shutil
-import os
 import sys
 
 def main():
-    path = sys.argv[1]
-    folder, _ = os.path.split(path)
-    temp_folder = folder + "\\temp"
+    if len(sys.argv) < 2:
+        print("Please provide the path to a .pptx file.")
+        sys.exit(1)
 
-    with ZipFile(path, 'r') as zObject:
+    file_path = Path(sys.argv[1])
+    folder = file_path.parents[0]
+    temp_folder = folder / "temp"
+
+    with ZipFile(file_path, 'r') as zObject:
         zObject.extractall(temp_folder)
 
-    file_list = glob.glob(temp_folder + "\\ppt\\media\\" + "*.m4a")
+    media_folder = temp_folder / "ppt" / "media"
+    file_list = media_folder.glob("*.m4a")
 
     total_time = 0
     for filename in file_list:
